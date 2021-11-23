61790c41b9e7a8fd24d029462d99db04fa245d61
Travis Duffy <90252209+zhoug0x@users.noreply.github.com>
2021-11-22 22:49:07 -0400

    brought in libraries, extended contract to inherit ERC-721

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
+import './libraries/Base64.sol';
+
 import 'hardhat/console.sol';
 
-contract MyEpicGame {
+contract MyEpicGame is ERC721 {
+	using Counters for Counters.Counter;
+
 	struct CharacterAttributes {
 		uint256 characterIndex;
 		uint256 stamina;
@@ -13,14 +20,16 @@ contract MyEpicGame {
 		string imageURI;
 	}
 
+	Counters.Counter private _tokenIds;
 	CharacterAttributes[] defaultCharacters;
 
+	// -------------------------------------------------------- constructor
 	constructor(
 		string[] memory characterNames,
 		string[] memory characterImageURIs,
 		uint256[] memory characterStamina,
 		uint256[] memory characterStrength
-	) {
+	) ERC721('Buildspace NFT Game', 'GAME') {
 		for (uint256 i = 0; i < characterNames.length; i += 1) {
 			defaultCharacters.push(
 				CharacterAttributes({
@@ -42,5 +51,15 @@ contract MyEpicGame {
 			);
 			console.log('img: %s\n---\n', c.imageURI);
 		}
+	}
+
+	// -------------------------------------------------------- functions
+	function tokenURI(uint256 _tokenId)
+		public
+		view
+		override
+		returns (string memory)
+	{
+		return Base64.encode(string(abi.encodePacked('{"id": "', _tokenId, '"}')));
 	}
 }
